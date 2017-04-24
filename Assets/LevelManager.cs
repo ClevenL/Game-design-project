d81504aaece631ad8f621c1b0b03e0a3409f23c1
ClevenL <cleven.lehispuu@gmail.com>
2017-04-24 17:18:07 +0300

    added basic UI

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name){
+		Debug.Log (name);
+		Application.LoadLevel(name);
+	}
+	public void QuitRequest(){
+		Debug.Log ("quit");
+		Application.Quit();
+	}
+}
