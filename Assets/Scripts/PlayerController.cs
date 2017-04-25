e90db218f11474bec053d9603d38a7d44c260468
ClevenL <cleven.lehispuu@gmail.com>
2017-04-25 17:12:54 +0300

    player movement animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
+	private Animator a;
+	private bool playerMoving;
+	private Vector2 lastMove;
 
-	// Use this for initialization
 	void Start () {
-		
+		a=GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if(Input.GetAxisRaw("Horizontal")>0.5f || Input.GetAxisRaw("Horizontal")<0.5f){
+
+		playerMoving=false;
+
+		if(Input.GetAxisRaw("Horizontal")>0.5f || Input.GetAxisRaw("Horizontal")<-0.5f){
 			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime,0f,0f));
+			playerMoving=true;
+			lastMove=new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
 		}
-		if(Input.GetAxisRaw("Vertical")>0.5f || Input.GetAxisRaw("Vertical")<0.5f){
+		if(Input.GetAxisRaw("Vertical")>0.5f || Input.GetAxisRaw("Vertical")<-0.5f){
 			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime,0f));
+			playerMoving=true;
+			lastMove=new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		}
+
+		a.SetFloat("MoveX",Input.GetAxisRaw("Horizontal"));
+		a.SetFloat("MoveY",Input.GetAxisRaw("Vertical"));
+		a.SetBool("PlayerMoving",playerMoving);
+		a.SetFloat("LastMoveX",lastMove.x);
+		a.SetFloat("LastMoveY",lastMove.y);
+
+		Debug.Log(playerMoving);
 	}
 }
