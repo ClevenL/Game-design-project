6cf3574cbac7a6cea0cbc7cb387661ed25a5f9af
ClevenL <cleven.lehispuu@gmail.com>
2017-04-25 15:17:46 +0300

    basic player movement

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 	public void LoadLevel(string name){
 		Debug.Log (name);
-		Application.LoadLevel(name);
+		SceneManager.LoadScene(name);
 	}
 	public void QuitRequest(){
 		Debug.Log ("quit");
