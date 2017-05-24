bfcdde2dc88bdea0f4e9602c96d23e09a563d643
ukupoder <uku.poder@gmail.com>
2017-05-24 14:25:22 +0300

    vestlus slimega. slime on static, et ta ei liiguks

---
+++ Assets/Scripts/dialogHolder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogHolder : MonoBehaviour {
+
+	public string dialogue;
+	private DialogueManager dMan;
+
+	public string[] dialogueLines;
+
+	// Use this for initialization
+	void Start () {
+		dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player") 
+		{
+			if (Input.GetKeyUp (KeyCode.Space)) 
+			{
+				//dMan.ShowBox (dialogue);
+
+				if (!dMan.dialogActive) 
+				{
+					dMan.dialogLines = dialogueLines;
+					dMan.currentLine = 0;
+					dMan.ShowDialogue();
+				}
+			}
+		}
+	}
+}
