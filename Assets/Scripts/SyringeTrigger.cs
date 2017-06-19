f847a1e8ca0c0bc8605c03ce278fb430b025a654
ClevenL <cleven.lehispuu@gmail.com>
2017-06-19 13:30:41 +0300

    minigame1 korras

--- Assets/Scripts/SyringeTrigger.cs
+++ Assets/Scripts/SyringeTrigger.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class SyringeTrigger : MonoBehaviour {
 
-	void OnTriggerEnter(Collider collider){
+	void OnTriggerEnter2D(Collider2D collider){
 	    if(collider.gameObject.tag == "syringe")
 	    {
 			ScoreScript.scoreValue --;
 	        Debug.Log("got hit!");
 	        Destroy(collider.gameObject);
+
+			if(ScoreScript.scoreValue < 0){
+				ScoreScript.scoreValue = 0;
+	        }
 
 	    }
 	}
