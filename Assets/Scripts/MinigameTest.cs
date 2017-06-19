12c65be9611a94d7032d90cb3d9ca1bf5eb553c2
ClevenL <cleven.lehispuu@gmail.com>
2017-06-19 10:56:01 +0300

    minigame systeem tootab

--- Assets/Scripts/MinigameTest.cs
+++ Assets/Scripts/MinigameTest.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class MinigameTest : MonoBehaviour {
     public Transform target;
-    public float rotate_speed = 5f;
+	public float timeLeft = 30f;
 	public float move_speed = 10f;
     void Update() {
         //Vector2 direction = target.position - transform.position;
         //float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
         //Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         //transform.rotation = Quaternion.Slerp(transform.rotation, rotation , rotate_speed*Time.deltaTime);
-		transform.position = Vector3.MoveTowards(transform.position, target.position, move_speed*Time.deltaTime);
+		//transform.position = Vector3.MoveTowards(transform.position, target.position, move_speed*Time.deltaTime);
+		timeLeft -= Time.deltaTime;
+		if(timeLeft <= 0.0f){
+			transform.Translate(0, 0, 0, Camera.main.transform);
+		}else{
+			transform.Translate(0, Time.deltaTime*(-2.5f), 0, Camera.main.transform);
+		}
     }
 }
--- Assets/Scripts/SyringeSpawn.cs
+++ Assets/Scripts/SyringeSpawn.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SyringeSpawn : MonoBehaviour {
 	public GameObject syringe;
+	//public GameObject finish;
 	GameObject syringeClone;
+	//GameObject finishClone;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,7 +14,10 @@ public class SyringeSpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 spawnPos = new Vector3(Random.Range(-3f, 3f), 3f);
-		//syringeClone = Instantiate(syringe, new Vector3(Random.Range(-3f, 3f), 3f)) as GameObject;
+		transform.position = new Vector3(Random.Range(-3f, 3f), 4f);
+		transform.rotation = Quaternion.Euler(0, 0, -90);
+		syringeClone = Instantiate(syringe, transform.position, transform.rotation) as GameObject;
+		//transform.position = new Vector3(transform.position.x, transform.position.y-7f);
+		//finishClone = Instantiate(finish, transform.position, transform.rotation) as GameObject;
 	}
 }
